ee338119dda7ef3e19df0c7d180584fab4de62cb
umarrkhan7 <umerkhanjadoon764@gmail.com>
2025-03-03 22:08:43 -0800

    commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
   const container = document.getElementById("container");
   const toggleButton = document.getElementById("toggleButton");
 
-  toggleButton.addEventListener("click", () => {
+  toggleButton.addEventListener("click", () => {      //switch 
     container.classList.toggle("active");
   });
 });
